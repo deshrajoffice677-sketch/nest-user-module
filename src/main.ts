0abb5bec3361661d5f38ec50d0d3a53b34b76615
deshrajoffice677-sketch <deshrajoffice677@gmail.com>
2025-12-24 16:41:59 +0530

    Add frontend url in  main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://react-user-module-ijbk0i2w1-deshrajoffice677-sketchs-projects.vercel.app/',
     credentials: true,
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
   });
