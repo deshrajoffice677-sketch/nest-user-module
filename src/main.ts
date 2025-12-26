0cb88c525f703ed5f3e30bcd45854d2ad5a3f091
deshrajoffice677-sketch <deshrajoffice677@gmail.com>
2025-12-26 10:48:59 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-app.enableCors({
-  origin: '*',
-  methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-});
-
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+  });
 
 
   // Enable Global Validation Pipe
